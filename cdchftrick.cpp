ac3ccca0a03b18eb08ef48526d17e6950da8e764
khushboosinghrecs <41840494+khushboosinghrecs@users.noreply.github.com>
2020-06-13 08:46:32 +0530

    Create cdchftrick.cpp

---
+++ cdchftrick.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <climits>
+int main()
+{
+    //max value of int 2147483647 which is more than 10^9 + 7
+    //INT_MAX > 10^9 + 7
+    //long max > 10^18
+    // ye sab yad kar lo
+    std::cout << INT_MAX << '\n';
+    return 0;
+}
