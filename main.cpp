e39bc75c539a76a8987f2552bd33b1c1190361ff
khushboosinghrecs <41840494+khushboosinghrecs@users.noreply.github.com>
2020-06-13 08:53:22 +0530

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <set>
+#include <utility>
+#include <unordered_map>
+#include <stack>
+#include <list>
+#include <queue>
+#include <algorithm>
+#define filei freopen("input.txt","r",stdin);
+#define fileo freopen("output.txt","w",stdout);
+#define fastio ios_base::sync_with_stdio(false);cin.tie(NULL);
+using namespace std;
+int main(void)
+{
+    filei;
+    fastio;
+    return 0;
+}
